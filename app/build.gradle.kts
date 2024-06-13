b0c7184d98708d22f120227d8ccad22e18cb6bba
Enel Almonte <enel_almonte@ucne.edu.do>
2024-06-12 20:37:21 -0400

    Adds retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,11 @@ dependencies {
     //  optional - Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
 
-
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
